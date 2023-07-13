3da9a37f0120dae75987097c2ed2e4bd02c57b1d
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-07-13 15:32:30 +0300

    fix deployment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-const Footer: React.FC<{} & React.PropsWithChildren> = (props) => {
-const {} = props;
- return (
-  <div>
-   
-  </div>
- );
+const Footer: React.FC = () => {
+  return <></>;
 };
 
 export default Footer;
