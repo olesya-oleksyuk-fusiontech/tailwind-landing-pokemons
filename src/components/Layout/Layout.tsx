8cf33cdc23e72e9ac05ae7a8ec3fc28bbc066b26
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-07-14 15:20:52 +0300

    change body background when navigating to pokemon page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 import Header from '@/components/Header';
 
 const Layout: React.FC<React.PropsWithChildren> = (props) => {
   const { children } = props;
 
+  const pathname = usePathname();
+
+  React.useEffect(() => {
+    if (pathname === '/pokemons/find') {
+      document.body.classList.add('bg-pokemon');
+    }
+    return () => {
+      document.body.classList.remove('bg-pokemon'); // Reset the body color
+    };
+  }, []);
+
+  const mainClasses = clsx('mx-auto max-w-4xl');
+
   return (
     <>
       <Header />
-      <main className="mx-auto max-w-4xl">{children}</main>
+      <main className={mainClasses}>{children}</main>
     </>
   );
 };
