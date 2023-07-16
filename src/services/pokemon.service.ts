4132b0bc046fb04642545d7f435586c54a844560
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-07-17 00:24:05 +0300

    add mock service to search a local array of pokemon species
    Reason: Since the PokéAPI doesn't have the support for searching pokémon;
    
    Mock search function returns a Promise that resolves with an artificial delay;

---
+++ src/services/pokemon.service.ts
@@ -0,0 +1,20 @@
+import { ALL_POKEMON_SPECIES } from '@/services/pokemonSpecies';
+
+export interface IPokemon {
+  name: string;
+  url: string;
+}
+
+export const PokemonService = {
+  async getAll(query: string): Promise<IPokemon[]> {
+    return new Promise((resolve) => {
+      const matchingPokemons = ALL_POKEMON_SPECIES.filter(({ name }) =>
+        name.includes(query.toLowerCase()),
+      );
+      // Artificial timeout for demonstration purposes
+      setTimeout(() => {
+        resolve(matchingPokemons);
+      }, 500);
+    });
+  },
+};
